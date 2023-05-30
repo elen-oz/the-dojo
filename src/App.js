8c1156078bc48948976017a37fc9df7569931c78
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-30 18:46:41 +0200

    feat: router & pages setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// styles
+import './App.css';
+
+// pages and components
+import Create from './pages/create/Create';
+import Dashboard from './pages/dashboard/Dashboard';
+import Login from './pages/login/Login';
+import Project from './pages/project/Project';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route
+              exact
+              path='/'
+            />
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/projects/:id'>
+              <Project />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
