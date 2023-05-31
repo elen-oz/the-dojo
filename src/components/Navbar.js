50fe13ea6b8e8b1d9ddc02949b2e624eb0e3ae32
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-31 14:59:00 +0200

    feat: conditional links in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // styles & images
 import './Navbar.css';
@@ -7,6 +8,7 @@ import Temple from '../assets/temple.svg';
 
 const Navbar = () => {
   const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
   return (
     <div className='navbar'>
@@ -19,31 +21,37 @@ const Navbar = () => {
           <span>The Dojo</span>
         </li>
 
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-        <li>
-          {isPending && (
-            <button
-              className='btn'
-              onClick={logout}
-              disabled
-            >
-              Logging out
-            </button>
-          )}
-          {!isPending && (
-            <button
-              className='btn'
-              onClick={logout}
-            >
-              Logout
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {isPending && (
+              <button
+                className='btn'
+                onClick={logout}
+                disabled
+              >
+                Logging out
+              </button>
+            )}
+            {!isPending && (
+              <button
+                className='btn'
+                onClick={logout}
+              >
+                Logout
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </div>
   );
