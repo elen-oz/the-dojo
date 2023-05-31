459351ef3129397530e3ec944e29c340e5529f16
Elena Ozerova <ozerova.lenka@gmail.com>
2023-05-31 11:47:50 +0200

    feat: loggin in users

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,59 @@
+import { useState } from 'react';
+
 // styles
 import './Login.css';
+import { useLogin } from '../../hooks/useLogin';
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { login, isPending, error } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <div>
+      <form
+        className='auth-form'
+        onSubmit={handleSubmit}
+      >
+        <h2>Login</h2>
+        <label>
+          <span>email:</span>
+          <input
+            required
+            type='email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+
+        <label>
+          <span>password:</span>
+          <input
+            required
+            type='password'
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+
+        {!isPending && <button className='btn'>Login</button>}
+        {isPending && (
+          <button
+            className='btn'
+            disabled
+          >
+            loading
+          </button>
+        )}
+        {error && <div className='error'>{error}</div>}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
