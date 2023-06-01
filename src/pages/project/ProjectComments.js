85727d8e5e1d340cd2a3ab142c3193be1f8185e0
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-01 18:44:49 +0200

    feat: add listing comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
+import Avatar from '../../components/Avatar';
 
 const ProjectComments = ({ project }) => {
   const [newComment, setNewComment] = useState('');
@@ -30,6 +31,22 @@ const ProjectComments = ({ project }) => {
   return (
     <div className='project-comments'>
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className='comment-author'>
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className='comment-date'>
+                <p>date here</p>
+              </div>
+              <div className='comment-content'>{comment.content}</div>
+            </li>
+          ))}
+      </ul>
 
       <form
         className='add-comment'
