300859ef0af176798e79cb87b35d6404d288c7f4
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-01 14:51:14 +0200

    feat: add project list

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,31 @@
+import { Link } from 'react-router-dom';
+import Avatar from './Avatar';
+
 // styles
+
 import './ProjectList.css';
 
 const ProjectList = ({ projects }) => {
   return (
-    <div>
+    <div className='project-list'>
       {projects.length === 0 && <p>No projects yet!</p>}
       {projects.map((project) => (
-        <div key={project.id}>{project.name}</div>
+        <Link
+          to={`projects/${project.id}`}
+          key={project.id}
+        >
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className='assignet-to'>
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
