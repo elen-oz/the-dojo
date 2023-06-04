a2faf8234e3cb7f3a3c08e9e4f8749ac3f1c9632
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-04 23:38:46 +0200

    refactor: update react-router-dom

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -4,7 +4,7 @@ import { useCollection } from '../../hooks/useCollection';
 import { timestamp } from '../../firebase/config';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { useFirestore } from '../../hooks/useFirestore';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // styles
 import './Create.css';
@@ -17,7 +17,7 @@ const categories = [
 ];
 
 const Create = () => {
-  const history = useHistory();
+  const navigate = useNavigate();
   const { addDocument, response } = useFirestore('projects');
   const { documents } = useCollection('users');
   const [users, setUsers] = useState([]);
@@ -71,7 +71,7 @@ const Create = () => {
     await addDocument(project);
 
     if (!response.error) {
-      history.push('/');
+      navigate('/');
     }
   };
 
